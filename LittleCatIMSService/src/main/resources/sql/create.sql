b9f21b8dff0c0bd6a7a90322e89158618106923d
amydady <215952449@qq.com>
2018-12-20 09:44:43 +0800

    asdf

--- LittleCatIMSService/src/main/resources/sql/create.sql
+++ LittleCatIMSService/src/main/resources/sql/create.sql
@@ -28,6 +28,7 @@ CREATE TABLE `t_basic_xiaoqu` (
 	`id` VARCHAR(125) NOT NULL,
 	`area` VARCHAR(255) NULL,
 	`name` VARCHAR(255) NOT NULL,
+	`code` VARCHAR(255) NOT NULL,
 	`enable` VARCHAR(1) NOT NULL DEFAULT 'Y',
 	
 	PRIMARY KEY (`id`),
